99342219661e7afa4ae64e42aa9ebf623ffdbd55
Gregory Fischer <gwhoisj@gmail.com>
2021-10-12 17:23:33 -0400

    Feat: add tables and migrations

---
+++ db/migrate/20211012211839_create_musician.rb
@@ -0,0 +1,12 @@
+class CreateMusician < ActiveRecord::Migration[5.2]
+  def change
+    create_table :musicians do |t|
+      t.integer :orc_id
+      t.string :name
+      t.boolean :hirable
+      t.integer :age
+      t.datetime :created_at
+      t.datetime :updated_at
+    end
+  end
+end
