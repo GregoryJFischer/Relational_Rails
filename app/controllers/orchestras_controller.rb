86efdba52f05dbe203dc506a2099196e918da4cd
Lesley Sanders <lesleyasanders@gmail.com>
2021-10-12 17:51:27 -0400

    Add create method for orchestras controller

--- app/controllers/orchestras_controller.rb
+++ app/controllers/orchestras_controller.rb
@@ -4,4 +4,19 @@ class OrchestrasController < ApplicationController
 
   def new
   end
+
+  def create
+    orchestra = Orchestra.new({
+      name: params[:orchestra][:name],
+      city: params[:orchestra][:city],
+      hiring: params[:orchestra][:hiring],
+      max_musicians: params[:orchestra][:max_musicians],
+      created_at: time.now,
+      updated_at: time.now
+      })
+
+    orchestra.save
+
+    redirect_to '/orchestras'
+  end
 end
