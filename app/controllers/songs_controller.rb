acf8af053fe704484169be4c9b791d1b1adff1eb
Lesley Sanders <lesleyasanders@gmail.com>
2021-10-12 17:41:35 -0400

    Add create method to songs controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,4 +4,19 @@ class SongsController < ApplicationController
 
   def new
   end
+
+  def create
+  song = Song.new({
+    composer_id: params[:song][:composer_id],
+    name: params[:song][:name],
+    public_domain: params[:song][:public_domain],
+    year_composed: params[:song][:year_composed],
+    created_at: time.now,
+    updated_at: time.now
+    })
+
+  song.save
+
+  redirect_to '/songs'
+  end
 end
