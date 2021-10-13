97b45596a7f4d20f61ef20e2c3c7b66cfaf824e2
Lesley Sanders <lesleyasanders@gmail.com>
2021-10-13 16:22:24 -0400

    Add update method for orchestras controller

--- app/controllers/orchestras_controller.rb
+++ app/controllers/orchestras_controller.rb
@@ -27,4 +27,19 @@ class OrchestrasController < ApplicationController
   def edit
     @orchestra = Orchestra.find(params[:id])
   end
+
+  def update
+    orchestra = Orchetra.find(params[:id])
+    orchestra.update({
+      name: params[:orchestra][:name],
+      city: params[:orchestra][:city],
+      hiring: params[:orchestra][:hiring],
+      max_musicians: params[:orchestra][:max_musicians],
+      created_at: time.now,
+      updated_at: time.now
+      })
+
+    orchestra.save
+    redirect_to "/orchestras/#{orchestra.id}"
+  end
 end
