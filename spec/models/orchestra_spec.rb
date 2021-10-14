1a6e99553f4ab66d30eb557b0c40f3377ffa21bf
Gregory Fischer <gwhoisj@gmail.com>
2021-10-14 18:06:56 -0400

    Feat: add setup for orchestra model test

--- spec/models/orchestra_spec.rb
+++ spec/models/orchestra_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Orchestra do
+  before :each do
+    @orchestra_1 = Orchestra.create(name: 'orchestra 1', 
+                                    city: 'city 1',
+                                    hiring: true,
+                                    max_musicians: 1)
+    @orchestra_2 = Orchestra.create(name: 'orchestra 2', 
+                                    city: 'city 2',
+                                    hiring: true,
+                                    max_musicians: 2)
+    @orchestra_3 = Orchestra.create(name: 'orchestra 3', 
+                                    city: 'city 3',
+                                    hiring: false,
+                                    max_musicians: 3)
+  end
   it {should have_many :musicians}
 end
