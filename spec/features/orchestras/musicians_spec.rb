3b9d47350a08423e5702764a02c82cfa943d38e7
Gregory Fischer <gwhoisj@gmail.com>
2021-10-14 17:50:43 -0400

    Feat: add testing functionality for parent/child

---
+++ spec/features/orchestras/musicians_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe 'Orchestra Musicians' do
+  before :each do
+    @orchestra = Orchestra.create!(name:          "New York Philharmonic",
+                                   city:          "New York City",
+                                   hiring:        true,
+                                   max_musicians: 100)
+    @musician_1 = Musician.create(name:         "Nancy Drew",
+                                  hirable:      true,
+                                  age:          22,
+                                  orchestra_id: @orchestra.id)
+    @musician_2 = Musician.create(name:         "Michael Scott",
+                                  hirable:      false,
+                                  age:          50,
+                                  orchestra_id: @orchestra.id)
+  end
+
+  it '#attributes' do
+    visit "/orchestras/#{@orchestra.id}/musicians"
+
+    expect(page).to have_content(@musician_1.name)
+    expect(page).to have_content(@musician_1.hirable)
+    expect(page).to have_content(@musician_1.age)
+    expect(page).to have_no_content(@musician_2)
+
+    expect(page).to have_content(@musician_2.name)
+    expect(page).to have_content(@musician_2.hirable)
+    expect(page).to have_content(@musician_2.age)
+    expect(page).to have_no_content(@musician_1)
+  end
+
+  it '#home' do
+    visit "/orchestras/#{@orchestra.id}/musicians"
+
+    expect(page).to have_link("Home")
+  end
+end
