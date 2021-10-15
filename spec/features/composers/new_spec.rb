fe3b99776de2bd491086377e348dc726e83ed281
Lesley Sanders <lesleyasanders@gmail.com>
2021-10-15 16:43:32 -0400

    Add heading test to composers new spec

--- spec/features/composers/new_spec.rb
+++ spec/features/composers/new_spec.rb
@@ -1,13 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'New' do
-  it 'nav' do
+  it 'has navigation' do
     visit "/composers/new"
-    
+
     expect(page).to have_link("Home")
     expect(page).to have_link("Orchestras")
     expect(page).to have_link("Musicians")
     expect(page).to have_link("Composers")
     expect(page).to have_link("Songs")
+  end
+
+  it 'has page heading' do
+    visit "/composers/new"
+
+    expect(page).to have_content("New Composers")
   end
 end
